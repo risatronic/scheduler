4f7615d3afa1413df5d6b3b5776452d4542a484b
risatronic <mcr.doig@gmail.com>
2020-09-04 13:14:11 -0700

    Implement CREATE state to be used when clicking add button in Empty component, and back function called by Form cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Button from "components/Button";
-// import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+
 
 export default function Form(props){
   const [name, setName] = useState(props.name || "");
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,25 +2,39 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
-import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+import useVisualMode from "../../hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  const create = function(){
+    transition(CREATE);
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && <Empty onAdd={create} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={back}
+          // onSave={}
         />
       )}
     </article>
